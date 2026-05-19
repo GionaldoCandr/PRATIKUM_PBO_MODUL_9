d1e2fe184db9b467c0820f4b55676e1074756b33
GionaldoCandr <gionaldocandrawansah@gmail.com>
2026-05-19 14:20:43 +0700

    pretest

---
+++ PRETEST/src/Harddisk.java
@@ -0,0 +1,9 @@
+class Harddisk {
+    private String tipe;
+    private int kapasitasGB;
+
+    public Harddisk(String tipe, int kapasitasGB) {
+        this.tipe = tipe;
+        this.kapasitasGB = kapasitasGB;
+    }
+}
---
+++ PRETEST/src/Monitor.java
@@ -0,0 +1,9 @@
+class Monitor {
+    private String merk;
+    private int ukuranInci;
+
+    public Monitor(String merk, int ukuranInci) {
+        this.merk = merk;
+        this.ukuranInci = ukuranInci;
+    }
+}
